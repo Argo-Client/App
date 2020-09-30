cbc995115c61d1ae36c248403439d4283a6dfec7
Guus van Meerveld <57101832+Guusvanmeerveld@users.noreply.github.com>
2020-09-30 17:36:01 +0200

    Introductie scherm af

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ import 'package:intl/intl.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flushbar/flushbar.dart';
+import 'package:intro_slider/dot_animation_enum.dart';
 
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:flutter_iconpicker/flutter_iconpicker.dart';
--- lib/src/ui/Introduction.dart
+++ lib/src/ui/Introduction.dart
@@ -11,20 +11,25 @@ class _Introduction extends State<Introduction> {
       account = Account(tokenSet);
       accounts.put(0, account);
       userdata.put("introduction", true);
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => App()));
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => App()));
       appState = _AppState();
       account.magister.refresh().then((_) async {
         appState.setState(() {});
         Agenda.of(_agendaKey.currentContext).setState(() {});
-        FlushbarHelper.createSuccess(message: "$account is succesvol ingelogd")..show(_agendaKey.currentContext);
+        FlushbarHelper.createSuccess(message: "$account is succesvol ingelogd")
+          ..show(_agendaKey.currentContext);
         await account.magister.downloadProfilePicture();
         appState.setState(() {});
       }).catchError((e) {
-        FlushbarHelper.createError(message: "Fout bij ophalen van gegevens:\n$e")..show(_agendaKey.currentContext);
+        FlushbarHelper.createError(
+            message: "Fout bij ophalen van gegevens:\n$e")
+          ..show(_agendaKey.currentContext);
         throw (e);
       });
     }).catchError((e) {
-      FlushbarHelper.createError(message: "Fout bij het inloggen:\n$e")..show(context);
+      FlushbarHelper.createError(message: "Fout bij het inloggen:\n$e")
+        ..show(context);
       throw (e);
     });
   }
@@ -36,23 +41,66 @@ class _Introduction extends State<Introduction> {
         builder: (context) => new IntroSlider(
           isShowSkipBtn: false,
           colorDot: Color.fromRGBO(255, 255, 255, .5),
+          sizeDot: 13.0,
+          renderNextBtn: Icon(
+            Icons.navigate_next,
+            color: Colors.white,
+          ),
+          typeDotAnimation: dotSliderAnimation.SIZE_TRANSITION,
           nameDoneBtn: "LOGIN",
           onDonePress: loginPress,
           slides: [
             Slide(
               title: "Argo",
-              description: "Een moderne magister app.\nStart snel op en is gebruiksvriendelijk",
+              pathImage: "assets/images/splash.png",
+              description:
+                  "Magister is van gister, Argo is van vandaag.\n\n Deze app is niet alleen mooi, hij is ook nog is zeer zwaar in de bèta dus verwacht niet veel.",
               backgroundColor: Colors.lightBlue,
             ),
             Slide(
+              title: "DISCLAIMER",
+              description:
+                  "Argo is niet gerelateerd en maakt geen onderdeel uit van SchoolMaster B.V.",
+              backgroundColor: Color(0xff3da4ab),
+            ),
+            Slide(
               title: "Login",
-              widgetDescription: Center(
-                child: RaisedButton(
-                  child: Text("Login"),
-                  onPressed: loginPress,
-                ),
+              widgetDescription: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: 150,
+                    ),
+                    child: Text(
+                      "Om de app te kunnen gebruiken, moet je eerst even inloggen.\n\n Je logt in via Magister dus je wachtwoord is helemaal veilig.",
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  Center(
+                    child: Container(
+                      height: 70,
+                      child: RaisedButton(
+                        elevation: 4,
+                        focusElevation: 6,
+                        color: Color(0xffe88679),
+                        child: Text(
+                          "Log nu in!",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 25,
+                          ),
+                        ),
+                        onPressed: loginPress,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              backgroundColor: Colors.teal,
+              backgroundColor: Color(0xfffe9c8f),
             ),
           ],
         ),
